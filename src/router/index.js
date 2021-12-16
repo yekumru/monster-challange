0f3f55e2ec50dadde24e726a9e984791785a9cdd
yekumru <yekumru@outlook.com>
2021-12-16 19:13:50 +0300

    hot fixed

--- src/router/index.js
+++ src/router/index.js
@@ -27,14 +27,6 @@ const router = new VueRouter({
 router.beforeEach((to, _, next) => {
     const isLoggedIn = isUserLoggedIn()
 
-    if (!canNavigate(to)) {
-        // Redirect to login if not logged in
-        if (!isLoggedIn) return next({ name: 'auth-login' })
-
-        // If logged in => not authorized
-        return next({ name: 'misc-not-authorized' })
-    }
-
     // Redirect if logged in
     if (to.meta.redirectIfLoggedIn && isLoggedIn) {
         const userData = getUserData()
